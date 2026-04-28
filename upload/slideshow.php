657ea84e5db67dda11acbf24074b3eb9aefa0caa
MadWay <64708726+madromas@users.noreply.github.com>
2026-04-27 21:47:58 -0400

    Refactor slideshow.php for PHP 8 and security
    Updated slideshow.php for PHP 8 compatibility and security improvements.

--- upload/slideshow.php
+++ upload/slideshow.php
@@ -1,61 +1,77 @@
 <?php
+// PHP 8: Strict error reporting is better for debugging, but keeping your preference:
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED);
+
 include('include/version.inc.php');
 require_once('include/Config.class.php');
 
 $globalpath = '';
 
-// FIX: If configfile is undefined or missing, force it to 'slideshowconfig.ini'
+// PHP 8 Null Coalescing
 $config = $_GET['configfile'] ?? 'slideshowconfig.ini';
-if ($config == 'undefined' || empty($config)) {
+if ($config === 'undefined' || empty($config)) {
     $config = 'slideshowconfig.ini'; 
 }
 
+// Basic security: prevent directory traversal
+$config = basename($config);
+
 if (!file_exists($config)) {
-    // If the specific ini isn't found, try the default as a last resort
     $config = 'slideshowconfig.ini';
 }
 
 $Config = new Config($config, false, false);
-$path = $Config->get('path') ?? '';
-$startheading = $Config->get('startheading') ?? '';
+$path = (string)($Config->get('path') ?? '');
+$startheading = (string)($Config->get('startheading') ?? '');
 
-$mytitle = ($startheading == 'foldername') ? $path : $startheading;
+$mytitle = ($startheading === 'foldername') ? $path : $startheading;
 
-$bgcol = $_GET['bgcol'] ?? $Config->get('bgcol') ?? 'FFFFFF';
-$textcol = $Config->get('textcol') ?? '000000';
+$bgcol = (string)($_GET['bgcol'] ?? $Config->get('bgcol') ?? 'FFFFFF');
+$textcol = (string)($Config->get('textcol') ?? '000000');
 
+// Prepare FlashVars for Ruffle
 $vars = $Config->listKeys();
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<title><?= htmlspecialchars((string)$mytitle) ?></title>
-<meta http-equiv="Content-Type" content="text/html">
-<script type="text/javascript" src="javascript/swfobject_source.js"></script>
-<script type="text/javascript" src="javascript/window.js"></script>
-<style type="text/css">
-    body, html { height: 100%; overflow: hidden; }
-    body { background-color: #<?= htmlspecialchars((string)$bgcol) ?>; margin: 0px; padding: 0px; }
-</style>
-</head>
-<body>
-    <div id="slideshow">slideshow module</div>
-    <script type="text/javascript">
-        var so = new SWFObject("slideshow.swf", "slideshow", "100%", "100%", "7", "#<?= $bgcol ?>");
-        so.addVariable("globalpath", "<?= $globalpath ?>");
-        so.addVariable("textcol", "<?= $textcol ?>");
-        so.addVariable("configfile", "<?= urlencode($config) ?>");
-        <?php 
-        foreach ($vars as $var) {
+$flashVarsArray = [
+    "globalpath" => $globalpath,
+    "textcol"    => $textcol,
+    "configfile" => $config
+];
+
+foreach ($vars as $var) {
     $var = trim((string)$var);
-    if(isset($_GET[$var])) {
-        $vvar = (string)$_GET[$var]; // Force string cast for PHP 8
-        echo 'so.addVariable("'. $var . '","' . urlencode($vvar).'");';
+    if (isset($_GET[$var])) {
+        $flashVarsArray[$var] = (string)$_GET[$var];
     }
 }
-        ?>
-        so.write("slideshow");
-    </script>
+
+// Build the query string for the FlashVars
+$flashVarsString = http_build_query($flashVarsArray);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?= htmlspecialchars((string)$mytitle) ?></title>
+    <style type="text/css">
+        body, html { height: 100%; overflow: hidden; background-color: #<?= htmlspecialchars($bgcol) ?>; margin: 0; padding: 0; }
+        #container { width: 100%; height: 100%; }
+    </style>
+    <script src="https://unpkg.com/@ruffle-rs/ruffle"></script>
+</head>
+<body>
+
+    <div id="container">
+        <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="100%" height="100%" id="slideshow">
+            <param name="movie" value="slideshow.swf" />
+            <param name="flashvars" value="<?= htmlspecialchars($flashVarsString) ?>" />
+            <param name="bgcolor" value="#<?= htmlspecialchars($bgcol) ?>" />
+            <object type="application/x-shockwave-flash" data="slideshow.swf" width="100%" height="100%">
+                <param name="flashvars" value="<?= htmlspecialchars($flashVarsString) ?>" />
+                <param name="bgcolor" value="#<?= htmlspecialchars($bgcol) ?>" />
+            <p>Flash content failed to load. Please ensure JavaScript is enabled.</p>
+            </object>
+            </object>
+    </div>
+
 </body>
 </html>
